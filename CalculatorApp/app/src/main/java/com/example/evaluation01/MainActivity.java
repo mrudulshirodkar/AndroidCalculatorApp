110943833a7d74168d8af21cd055d016b61710f5
Mrudul Shirodkar <mrudulshirodkar@gmail.com>
2024-01-27 23:02:16 -0500

    Starter Code for the Calculator app

---
+++ CalculatorApp/app/src/main/java/com/example/evaluation01/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.evaluation01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "Eval01";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
